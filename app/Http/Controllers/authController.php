cf14f21f81bb67bc6d662a7ecd48e09bc60aa02c
Junior Iat <junioriat.oficial@gmail.com>
2026-09-10 16:26:30 -0300

    Atualização na mensagem de erro dos inputs, inclusão da base de dados

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class authController extends Controller
 {
@@ -15,17 +16,32 @@ class authController extends Controller
     {
         // form validation
         $request->validate(
+            //rules
             [
-                'text_username' => 'required',
-                'text_password' => 'required'
-                ]
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            //error messages
+            [
+                'text_username.required' => 'O Username é obrigatório',
+                'text_username.email' => 'O Username deve ser um email válido',
+                'text_password.required' => 'A Password é obrigatória',
+                'text_password.min' => 'A Password deve ter no mínimo :min caracteres',
+                'text_password.max' => 'A Password deve ter no máximo :max caracteres'
+            ]
         );
 
         // get user input
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo 'OK!';
+        // test database connection
+        try {
+            DB::connection()->getPdo();
+            echo "CONECTADO COM SUCESSO";
+        } catch (\PDOException $e) {
+            echo "ERRO AO CONECTAR COM O BANCO DE DADOS: " . $e->getMessage();
+        }
     }
 
     public function logout()
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Http\Controllers\authController;
 use App\Http\Controllers\mainControler;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // auth routes
 Route::get('login', [authController::class, 'login']);
 Route::post('loginSubmit', [authController::class, 'loginSubmit']);
 Route::get('logout', [authController::class, 'logout']);
+
